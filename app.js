b0b97fb20c5374c22d7542d4062ede9b53806f44
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-23 16:12:32 +0900

    lab 4.2

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+const rootDir = require("./util/path");
+
 const express = require("express");
+const path = require("path");
+const exp = require("constants");
 
 const app = express();
 
+app.use(express.static(path.join(rootDir, "public")));
+
 app.use("/users", (req, res, next) => {
-  console.log("Route user");
-  res.send("<p>The middleware that handles just/users</p>");
+  res.sendFile(path.join(rootDir, "views", "users.html"));
 });
 app.use("/", (req, res, next) => {
-  console.log("Route default");
-  res.send("<p>The middleware that handles just</p>");
+  res.sendFile(path.join(rootDir, "views", "index.html"));
 });
 
 app.listen(3001);
