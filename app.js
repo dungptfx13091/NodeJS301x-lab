e17ee29ece165afa1ac6c190f7473c54d24a1fbe
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-23 15:34:07 +0900

    lab 4.1

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3001);
+app.use("/users", (req, res, next) => {
+  console.log("Route user");
+  res.send("<p>The middleware that handles just/users</p>");
+});
+app.use("/", (req, res, next) => {
+  console.log("Route default");
+  res.send("<p>The middleware that handles just</p>");
+});
+
+app.listen(3001);
