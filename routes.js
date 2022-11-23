7064104c60f5661fa8457aaadf0212854044d4c6
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-23 14:35:03 +0900

    lab 3.1

--- routes.js
+++ routes.js
@@ -1,32 +1,19 @@
 const fs = require("fs");
 
-const Users = [];
-
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-
   if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>Enter Users</title><head>");
+    res.write("<head><title>Enter Message</title><head>");
     res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="users"><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
-  if (url === "/users") {
-    res.write("<html>");
-    res.write("<head><title>Users</title><head>");
-    let writeres = "";
-    for (i = 0; i < Users.length; i++) {
-      writeres += "<li>" + Users[i] + "</li>";
-    }
-    res.write("<body><ul>" + writeres + "</ul></body>");
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/create-user") {
+
+  if (url === "/message" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
       console.log(chunk);
@@ -34,13 +21,20 @@ const requestHandler = (req, res) => {
     });
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const user = parsedBody.split("=")[1];
-      Users.push(user);
-      res.statusCode = 302;
-      res.setHeader("Location", "/users");
-      return res.end();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
   }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 };
 
 // module.exports = {
